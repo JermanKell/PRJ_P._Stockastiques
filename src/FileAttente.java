f86e5386a13bdfa949eb18d9d105e930657abbbf
arccho <xzeno@orange.fr>
2018-11-12 11:10:01 +0100

    fa

--- src/FileAttente.java
+++ src/FileAttente.java
@@ -1,4 +1,24 @@
 public class FileAttente {
+
+    //arrivée
+    private int λ;
+
+    //service
+    private int μ;
+
+    //nb serveurs
+    private int s;
+
+    //
+    private double ρ;
+
+    public FileAttente(int λ, int μ, int s)
+    {
+        this.λ = λ;
+        this.μ = μ;
+        this.s = s;
+    }
+
     public static void main(String [] args)
     {
 
