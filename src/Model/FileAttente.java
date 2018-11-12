4589ea992a7062840538be12ff74b14e515d1c52
arccho <xzeno@orange.fr>
2018-11-12 11:45:52 +0100

    w & wq mm1

--- src/Model/FileAttente.java
+++ src/Model/FileAttente.java
@@ -2,37 +2,44 @@ package Model;
 
 public class FileAttente {
 
-    //arrivée
+    //arrivee
     private int λ;
 
     //service
     private int μ;
 
-    //nb serveurs
-    private int s;
-
     //
     private double ρ;
 
-    public FileAttente(int λ, int μ, int s)
+    public FileAttente(int λ, int μ)
     {
         this.λ = λ;
         this.μ = μ;
-        this.s = s;
     }
 
     //nb moyen de clients dans la file
     public double Lq()
     {
-
         return ((Math.pow(λ, 2))/(μ*(μ-λ)));
     }
 
-    //nb moyen L de clients dans le système
+    //nb moyen L de clients dans le systeme
     public double L()
     {
         return (λ/(μ-λ));
     }
-    
+
+    //duree moyenne d'attente d'1 client quelquonque dans la file
+    public double Wq()
+    {
+        return (W()-(1/μ));
+    }
+
+    //duree moyenne d'attente dans le systeme
+    public double W()
+    {
+        return (1/(μ-λ));
+    }
+
 
 }
